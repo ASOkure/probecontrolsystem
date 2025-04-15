fb4905f5be6e5f384b51ce54531279c594229cf6
akanimo sam okure <akan.okure@gmail.com>
2025-04-15 09:03:06 +0100

    feat: add and implement Probe controller with REST endpoint to control probe

---
+++ src/main/java/com/mavericsystems/oceanprobecontrol/controller/ProbeController.java
@@ -0,0 +1,33 @@
+package com.mavericsystems.oceanprobecontrol.controller;
+
+import com.mavericsystems.oceanprobecontrol.dto.ProbeCommandRequest;
+import com.mavericsystems.oceanprobecontrol.dto.ProbeCommandResponse;
+import com.mavericsystems.oceanprobecontrol.model.Grid;
+import com.mavericsystems.oceanprobecontrol.model.Position;
+import com.mavericsystems.oceanprobecontrol.model.Probe;
+import com.mavericsystems.oceanprobecontrol.service.CommandProcessor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.Set;
+
+public class ProbeController {
+
+    private final CommandProcessor processor;
+    private final Grid grid;
+
+    public ProbeController() {
+
+        this.grid = new Grid(10, 10, Set.of(new Position(2, 2)));
+        this.processor = new CommandProcessor(grid);
+    }
+
+    @PostMapping("/command")
+    public ResponseEntity<ProbeCommandResponse> controlProbe(@RequestBody ProbeCommandRequest request) {
+        Probe probe = new Probe(new Position(request.startX, request.startY), request.startDirection);
+        processor.processCommands(probe, request.commands);
+        return ResponseEntity.ok(new ProbeCommandResponse(probe.getCurrentPosition(), probe.getVisitedPositions()));
+    }
+
+}
