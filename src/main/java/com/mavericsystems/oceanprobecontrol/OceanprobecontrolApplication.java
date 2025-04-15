2d699f47c68d6b5d37b3faaf77c4d1f70adc0c1a
akanimo sam okure <akan.okure@gmail.com>
2025-04-15 07:03:00 +0100

    project setup

---
+++ src/main/java/com/mavericsystems/oceanprobecontrol/OceanprobecontrolApplication.java
@@ -0,0 +1,13 @@
+package com.mavericsystems.oceanprobecontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OceanprobecontrolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OceanprobecontrolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mavericsystems/oceanprobecontrol/OceanprobecontrolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mavericsystems.oceanprobecontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OceanprobecontrolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
